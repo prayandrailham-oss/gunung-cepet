c39d42f0156e7d253e832d5f2e68b3f0b7eff814
prayandrailham-oss <prayandrailham@gmail.com>
2025-09-19 13:21:40 +0700

    Update autosummit.lua

--- autosummit.lua
+++ autosummit.lua
@@ -572,7 +572,7 @@ createAutoSummit("MOUNT PARVATRA", {
     CFrame.new(525.04, 508.44, 2381.48),
     CFrame.new(581.65, 651.69, 2654.95),
 }, 1, 0, {
-    autoRespawn = true
+    autoRespawn = false
 })
 
 -- MOUNT GAMPIL (NO AUTORESPAWN + TELEPORT SECEPAT MUNGKIN)
