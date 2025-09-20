50454e31f31bafa926cb7cafe2bb58ca92f67ea1
prayandrailham-oss <prayandrailham@gmail.com>
2025-09-20 08:12:01 +0700

    Update autosummit.lua

--- autosummit.lua
+++ autosummit.lua
@@ -632,8 +632,8 @@ createAutoSummit("MOUNT AMBANG", {
 createAutoSummit("MOUNT IJOO", {
     CFrame.new(476.19, 236.44, 489.94),      -- CP 1
     CFrame.new(-132.51, 533.44, 800.25)      -- SUM
-}, 1, 1, {   -- delay antar posisi = 2 detik
-    autoRespawn = true
+}, 1, 0, {   -- delay antar posisi = 2 detik
+    autoRespawn = false
 })
 
 createAutoSummit("MOUNT YAREU", {
