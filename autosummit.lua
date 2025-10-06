5e0c8973647c6e13b0a41fc2f09902f330945b28
prayandrailham-oss <prayandrailham@gmail.com>
2025-10-06 23:10:56 +0700

    Update autosummit.lua

--- autosummit.lua
+++ autosummit.lua
@@ -637,7 +637,7 @@ createAutoSummit("MOUNT HERENSUGEA", {
 })
 
 createAutoSummit("MOUNT CRATCH", {
-    CFrame.new(-495.55, 249.28, 946.51),      -- CP 1
+    CFrame.new(-914.11, 248.73, 1113.08),      -- CP 1
     CFrame.new(773.52, 1384.44, -590.52),    -- SUM
 }, 1, 0.5, {   -- delay antar posisi = 2 detik
     autoRespawn = false
